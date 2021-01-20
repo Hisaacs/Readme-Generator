0f171b5a5a52bcf0f8866d875a120deed426cee0
Hamish Isaacs <hamisaacs@gmail.com>
2021-01-20 14:45:50 +0800

    Added questions

--- index.js
+++ index.js
@@ -5,6 +5,45 @@ const fs = require("fs");
 // array of questions for user
 const questions = [
 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide description of your project:'
+    },
+
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project:'
+    },
+
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'Please provide usage information for your project:'
+    },
+
+    {
+        type: 'input',
+        name: 'contributions guidelines',
+        message: 'Please advise any contributions guidelines you have made:'
+
+    },
+
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'Please advise test instructions for your project:'
+    }
+
+    
 ];
 
 // function to write README file
