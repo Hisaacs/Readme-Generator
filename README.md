f9ebb24ff32bed9713879087126d269d0d8b8a37
Hamish Isaacs <hamisaacs@gmail.com>
2021-01-21 16:28:09 +0800

    Updated generateMarkdown.js and index.js

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
-# README-Generator
-  [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/undefined/undefined)
 
-  #Description
-  undefined
+# README-Generator
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/Hisaacs/README-Generator)
+
+# Description
+
+This is a command-line application that dynamically generates a professional README.md from user input, using Inquirer package. Also, an application that allows the user to quickly and easily genearate a README.md file for a project.
+
+# Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+# Installation
+
+The following necessary dependencies must be installed to run the application properly: Firstly, Fork the GitHub repository and copy it to your local machine. Secondly, open Gitbash or terminal and run npm i inquirer and npm i fs commands, then run node index.js to begin your application.
+
+# Usage
+
+​This application is used for To use this application. First you will need to install inquirer and fs (file system) dependencies.
+
+# License
+
+This project is license under the MIT License license.
+
+# Contributing
+
+​Contributors: I have contributed to this project.
+
+# Tests
+
+To run tests, you need to run the following command: To test functionality for your application, run node index.js in your terminal.
+
+# Questions
+
+If you have any questions about the repo, open an issue or contact Hisaacs directly hamisaacs@gmail.com.
 
--- index.js
+++ index.js
@@ -1,64 +1,86 @@
-// importing required files
+// imports
 const inquirer = require("inquirer");
 const generatorMarkdown = require("./utils/generateMarkdown")
-const fs = require("fs");
+const fs = require("fs"); 
 
-// An array of questions for user
+// Questions asked to the user
 const questions = [
+     
+    {
+      type: "input",
+      message: "What is the title for your project?",
+      name: "Title",
+    },
+    {
+      type: "input",
+      message: "Please provide description of your project:",
+      name: "Description",
+    },
+    {
+      type: "input",
+      message: "What necessary dependencies must be installed to run this app?",
+      name: "Installation",
+    },
+    {
+      type: "input",
+      message: "Please provide information usage for your project:",
+      name: "Usage",
+    },
+    {
+      type: "list",
+      message: "What license was used for this README?",
+      name: "License",
+      choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0',
+      'MIT License', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License',
+      'Boost Software License 1.0']
+  
+    },
+  
+    {
+      type: "input",
+      message: "Please advise any contributions you have made:",
+      name: "Contributor",
+    },
+    {
+      type: "input",
+      message: "Please advise your test opporation performed for your project:",
+      name: "Test",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "UserName",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "Email",
+      },
 
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide description of your project:'
-    },
-    {
-        type: 'input',
-        name: 'installation instructions',
-        message: 'Please provide installation instructions for your project:'
-    },
-    {
-        type: 'input',
-        name: 'usage information',
-        message: 'Please provide information usage for your project:'
-    },
-    {
-        type: 'input',
-        name: 'contributions guidelines',
-        message: 'Please advise any contributions you have made:'
 
-    },
-    {
-        type: 'input',
-        name: 'test instructions',
-        message: 'Please advise your test opporation performed for your project:'
-    }
-    
-    
-];
+  ];
+  
 
-// function to write README file
+// Writing to a file 
 function writeToFile(fileName, data) {
 
-    fs.writeFile("./"+fileName, data, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log ("Successfully wrote: " + fileName);
-    });
+fs.writeFile("./"+fileName, data, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log ("Successfully wrote: " + fileName);
+});
+
 }
 
-// function to initialize program
+// initialization function
 function init() {
-    inquirer.prompt(questions)
-    .then(function(data) {
-      writeToFile("README.md", generatorMarkdown(data));
-    });
+  inquirer.prompt(questions)
+  .then(function(data) {
+    writeToFile("README.md", generatorMarkdown(data));
+  });
 }
 
-// function call to initialize program
+
+// run the app
 init();
