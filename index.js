412db7fe88d320721d6d5b7e03d592dfc76028b9
Hamish Isaacs <hamisaacs@gmail.com>
2021-01-21 10:44:37 +0800

    Added more prompts for user

--- index.js
+++ index.js
@@ -11,7 +11,33 @@ const questions = [
         name: 'title',
         message: 'What is the title of your project?'
     },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'Please provide installation instructions for your project:'
+    },
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'Please provide information usage for your project:'
+    },
+    {
+        type: 'input',
+        name: 'contributions guidelines',
+        message: 'Please advise any contributions you have made:'
 
+    },
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'Please advise your test opporation performed for your project:'
+    }
+    
     
 ];
 
